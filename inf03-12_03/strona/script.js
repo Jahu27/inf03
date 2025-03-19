239fb80ba55917461089173e68a647b0aeff30cd
Jahu27 <122030650+Jahu27@users.noreply.github.com>
2025-03-19 10:51:45 +0100

    Delete inf03-12_03 directory

--- inf03-12_03/strona/script.js
+++
@@ -1,7 +0,0 @@
-
-document.getElementById("calc").addEventListener("click",function(){
-    var amount = document.getElementById("size").value;
-    var cansOf = Math.ceil(amount/4);
-    document.getElementById("anwser").innerText = "Liczba potrzebnych puszek: " 
-    + cansOf;
-})
